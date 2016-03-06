17ce08cb856cb2d302b2bfc9e048d08050a1fd93
Olimpio3oo <victorolimpio@id.uff.br>
2016-03-05 21:13:22 -0300

    Versão completa não funcional
    Problemas com estrututras de dados e armazenamento de informações.

--- TrabalhoPC/TrabalhoPC/Main.cs
+++ TrabalhoPC/TrabalhoPC/Main.cs
@@ -1,18 +1,19 @@
 ﻿using System;
+using System.Collections;
 using TrabalhoPC;
 
 public static class Teste
 {
     static void Main(String[] args)
     {
-        Console.WriteLine("Passou");
-        RungeKutta4 RK = new RungeKutta4(0.25, 0.5, 0);
-        Ponto[] resp = RK.Executa();
-        for (int i = 0; i < resp.Length; i++)
-        {
-            Console.WriteLine("("+resp[i].X +","+ resp[i].Y+")");
-        }
-        System.Threading.Thread.Sleep(50000);
+        ArrayList resp;
+        VariableStep VS = new VariableStep();
+        resp = VS.Executa(0, 2, 0.5, 0.05, 0.25, 0.01);
+        
+        
+        
+        
+        //System.Threading.Thread.Sleep(50000);
         
         
         
--- TrabalhoPC/TrabalhoPC/VariableStep.cs
+++ TrabalhoPC/TrabalhoPC/VariableStep.cs
@@ -1,23 +1,35 @@
 ﻿using System;
 using TrabalhoPC;
+using System.Collections;
 public class VariableStep
 
 {
-    double A, B, Alfa, Sigma, Tol, Hmax, Hmin, Wp, Wc;
+    double A, B, Alfa, Sigma, Tol, Hmax, Hmin, Wp, Wc, q;
 
-    double t, w, h;
-    
+    double t, h;
+    double[] saida;
     Boolean Flag, Last, Nflag;
     RungeKutta4 RK4 = new RungeKutta4();
     Ponto[] RespRK = new Ponto[4];
     int i;
-   
-	public VariableStep()
 
-	{
+    
+
+    public ArrayList Executa(double extremoA, double extremoB, double alpha, double tolerancia, double Hmax, double Hmin)
+    {
+        A = extremoA;
+        B = extremoB;
+        Alfa = alpha;
+        this.Hmax = Hmax;
+        this.Hmin = Hmin;
+        Tol = tolerancia;
+        
+        
+        
         // saida[0] = t, saida[1] = w, saida[2] = h
-        double temp1,temp2,temp3,temp4;
-        w = Alfa;
+        double temp1, temp2, temp3, temp4;
+        double w = Alfa;
+        ArrayList ultimateResp = new ArrayList();
         t = A;
         h = Hmax;
         Flag = true;
@@ -25,26 +37,132 @@ public class VariableStep
         RespRK = RK4.Executa(h, w, t);
         Nflag = true;
         i = 4;
-        t = RespRK[2].X + w;
+        t = RespRK[2].X + h;
         while (Flag)
         {
-            temp1 = RK4.CalculaDiferencial(RespRK[i - 1].X, RespRK[i - 1].Y);
+            temp1 = RK4.CalculaDiferencial(A, Alfa);
             temp2 = RK4.CalculaDiferencial(RespRK[i - 2].X, RespRK[i - 2].Y);
             temp3 = RK4.CalculaDiferencial(RespRK[i - 3].X, RespRK[i - 3].Y);
             temp4 = RK4.CalculaDiferencial(RespRK[i - 4].X, RespRK[i - 4].Y);
-            Wp = RespRK[i - 1].Y + (h/24) * (55 * temp1 - 59 * temp2 + 37 * temp3 - 9 * temp4);
+            Wp = RespRK[i - 2].Y + (h / 24) * (55 * temp1 - 59 * temp2 + 37 * temp3 - 9 * temp4);
             temp1 = RK4.CalculaDiferencial(t, Wp);
-            temp2 = RK4.CalculaDiferencial(RespRK[i - 1].X, RespRK[i - 1].Y);
-            temp3 = RK4.CalculaDiferencial(RespRK[i - 2].X, RespRK[i - 2].Y);
-            temp4 = RK4.CalculaDiferencial(RespRK[i - 3].X, RespRK[i - 3].Y);
-            Wc = RespRK[i - 1].Y + (Hmax / 24) * (9 * temp1 + 19 * temp2 - 5 * temp3 + temp4);
-            Sigma = 19 * Math.Abs(Wc - Wp) / 270 * h);
+            temp2 = RK4.CalculaDiferencial(RespRK[i - 2].X, RespRK[i - 2].Y);
+            temp3 = RK4.CalculaDiferencial(RespRK[i - 3].X, RespRK[i - 3].Y);
+            temp4 = RK4.CalculaDiferencial(RespRK[i - 4].X, RespRK[i - 4].Y);
+            Wc = RespRK[i - 2].Y + (h / 24) * (9 * temp1 + 19 * temp2 - 5 * temp3 + temp4);
+            Sigma = 19 * Math.Abs(Wc - Wp) / 270 * h;
             if (Sigma <= Tol)
             {
                 w = Wc;
-                
+
+                if (Nflag)
+                {
+                    saida = new double[4];
+                    saida[0] = 0;
+                    saida[1] = A;
+                    saida[2] = RespRK[0].Y;
+                    saida[3] = h;
+                    ultimateResp.Insert(0, saida);
+                    for (int j = i - 3; j < i-1; j++)
+                    {
+                        saida = new double[4];
+                        saida[0] = j;
+                        saida[1] = RespRK[j].X;
+                        saida[2] = RespRK[j].Y;
+                        saida[3] = h;
+                        ultimateResp.Insert(j, saida);
+                    }
+                }
+                else
+                {
+                    saida = new double[4];
+                    saida[0] = i;
+                    saida[1] = RespRK[i].X;
+                    saida[2] = RespRK[i].Y;
+                    saida[3] = h;
+                    ultimateResp.Insert(i, saida);
+                }
+                if (Last)
+                {
+                    Flag = false;
+
+                }
+                else
+                {
+                    i++;
+                    Nflag = false;
+
+                    if ((Sigma <= 0.1 * Tol) || ((RespRK[2].X + h) > B))
+                    {
+                        q = Math.Pow((Tol / (2 * Sigma)), 1 / 4);
+
+                        if (q > 4)
+                        {
+                            h = 4 * h;
+                        }
+                        else
+                        {
+                            h = q * h;
+                        }
+
+                        if (h > Hmax)
+                        {
+                            h = Hmax;
+                        }
+
+                        if ((RespRK[2].X + 4 * h) > B)
+                        {
+                            h = (B - RespRK[2].X) / 4;
+                            Last = true;
+                        }
+
+                        RespRK = RK4.Executa(h, RespRK[2].Y, RespRK[2].X);
+
+                        Nflag = true;
+                        i = i + 3;
+                    }
+                    else
+                    {
+                        q = Math.Pow(Tol / (2 * Sigma), 1 / 4);
+                        if (q < 0.1)
+                        {
+                            h = 0.1 / h;
+                        }
+                        else
+                        {
+                            h = q * h;
+                        }
+
+                        if (h < Hmin)
+                        {
+                            Flag = false;
+                            Console.WriteLine("Him ultrapassado");
+                        }
+                        else
+                        {
+                            if (Nflag)
+                            {
+                                i = i - 3;
+                            }
+
+                            RespRK = RK4.Executa(h, RespRK[2].Y, RespRK[2].X);
+                            i = i + 3;
+                            Nflag = true;
+                        }
+
+                        t = RespRK[i - 1].X + h;
+                    }
+                }
             }
         }
-        
-	}   
+
+
+
+
+
+
+
+
+        return ultimateResp;
+    }
 }
